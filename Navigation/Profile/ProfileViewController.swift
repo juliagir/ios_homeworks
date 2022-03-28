c403c7e9911c8f042da2f10dfe17439dbb4dd39e
Julia Ghirnic <poslik@gmail.com>
2022-03-28 19:12:37 +0300

    Выполнила основное и дополнительное задания.

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    
+    let header = ProfileHeaderView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
-        self.title = "Profile"
-
+        view.backgroundColor = .lightGray
+        self.view.addSubview(header)
+        title = "Profile"
     }
-
+    
+    override func viewWillLayoutSubviews() {
+        
+        header.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            header.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+            header.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+            header.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            header.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
